179f21511f3d27781cd3c0b62e3abfba462acb09
Ben <brbryant2639@gmail.com>
2021-08-24 15:07:51 -0500

    JS Intro notes

---
+++ walkthroughs/intro.js
@@ -0,0 +1,16 @@
+// [declaration keyword] [identifier] = [value]
+
+// Declaration Keywords
+var y; // declare a variable that is function (local) scoped. can be reassigned
+let x; // declare a variable that is block (local) scoped. can be reassigned
+const months = 12; // declare a variable that is block (local) scoped. cannot be reassigned
+
+// Data Types
+let notDefined; // undefined
+let empty = null; // null
+let year = 2021; // number
+let name = "Ben"; // string
+let largeNumber = 12345678901234567890n; // BigInt
+let yesOrNo = true; // boolean
+("unique"); // only a symbol in the context of structured data
+
