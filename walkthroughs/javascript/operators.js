26da4c52ea16d7953718b888f089ab1c396d636e
Ben <brbryant2639@gmail.com>
2021-08-26 08:49:06 -0500

    Added notes from js operators

--- walkthroughs/javascript/operators.js
+++ walkthroughs/javascript/operators.js
@@ -1,9 +1,21 @@
-let password = "password";
+// Arithmetic Operators
+1 + 2; // addition
+1 - 2; // subtraction
+1 * 2; // multiplication
+1 / 2; // division
+1 % 2; // division remainder
+1 ** 2; // exponentiation
 
-let userCredientials = "1kd;lKKdjgkl;!";
+// Comparison Operators
+1 > 2; // greater than
+1 >= 2; // greater than or equal to
+1 < 2; // less than
+1 <= 2; // less than or equal to
+1 == 2; // equal to (loose with type coercion)
+1 != 2; // inequal to (loose with type coercion)
+1 === 2; // equal to (strict without type coercion)
+1 !== 2; // inequal to (strict without type coercion)
 
-if (password === userCredientials) { 
-  return "logged in";
-} else {
-  return "password not correct";
-}
+// Logical Operators
+1 == 2 && 1 > 2; // Logical AND, Both expressions must equate to TRUE
+1 == 2 || 1 > 2; // Logical OR, Only one expression must equate to TRUE
