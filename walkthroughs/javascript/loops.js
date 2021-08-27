18f55eb56cfe4ce48ce8bf0b3d94f97d37823804
Ben <brbryant2639@gmail.com>
2021-08-27 15:05:38 -0500

    Updated with ex 1 for functions

--- walkthroughs/javascript/loops.js
+++ walkthroughs/javascript/loops.js
@@ -1,6 +1,15 @@
 console.log("Script attached");
 
 // Exercise 1
+function printOddsWithinRange(start, end) {
+  for (let i = start; i <= end; i++) {
+    if (i % 2 == 1) {
+      console.log(i);
+    }
+  }
+}
+
+printOddsWithinRange(50, 100);
 
 // Exercise 2
 
