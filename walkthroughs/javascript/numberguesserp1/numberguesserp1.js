6a952a1f60904cbeb066b80e68be7f7dc52c5d37
Ben Bryant <brbryant2639@gmail.com>
2021-09-02 11:35:31 -0500

    Number guessing game p1 solution

--- walkthroughs/javascript/numberguesserp1/numberguesserp1.js
+++ walkthroughs/javascript/numberguesserp1/numberguesserp1.js
@@ -1,20 +1,80 @@
 console.log("Hello World!");
 
-let correctNumber = Math.floor(Math.random() * 1000) + 1;
-let isPlaying = true;
+/*
+Generate a Random Number at the beginning (start)
+Prompt user for a guess
+Make sure that the guess is a number between 1 and n
+Evaluate Number
+Give Feedback
+*/
 
-while (isPlaying) {
-    let guess = parseInt(prompt("Guess a whole number between 1 and 100:"));
-    if (isNaN(guess) || guess < 1 || guess > 1000) {
-        alert("You didn't give me a whole number between 1 and 100 :(");
-    } else {
-        if (guess > correctNumber) {
-            alert("Too high");
-        } else if (guess < correctNumber) {
-            alert("Too low");
-        } else {
-            isPlaying = false;
-            alert("Congratulations!!!");
-        }
+let mode = null;
+const limit = {
+  easy: 10,
+  medium: 100,
+  hard: 1000,
+};
+const modes = ["easy", "medium", "hard"];
+
+startGame();
+
+/* =========== DEFINITIONS =========== */
+
+function startGame() {
+  let isPlaying = true;
+  decideMode();
+  let correctNumber = Math.floor(Math.random() * limit[mode]) + 1;
+  playGame();
+
+  function playGame() {
+    let guess = prompt(
+      `Guess a whole number between 1 and ${limit[mode]}:\nType quit to stop.`
+    );
+    if (!hasQuit(guess)) {
+      evaluateGuess(parseInt(guess));
     }
+    if (isPlaying) {
+      playGame();
+    }
+  }
+
+  function evaluateGuess(guess) {
+    if (isNaN(guess) || guess < 1 || guess > limit[mode]) {
+      alert(
+        `You didn't give me a whole number between 1 and ${limit[mode]} :(\nType quit to stop.`
+      );
+    } else {
+      if (guess > correctNumber) {
+        alert("Too high");
+      } else if (guess < correctNumber) {
+        alert("Too low");
+      } else {
+        isPlaying = false;
+        alert("Congratulations!!!");
+        let replay = prompt("Would you like to play again (yes/no):");
+        if (replay.toLowerCase().trim()[0] == "y") {
+          startGame();
+        }
+      }
+    }
+  }
+
+  function hasQuit(input) {
+    if (input == "quit") {
+      isPlaying = false;
+      alert("See you later!");
+      return true;
+    } else return false;
+  }
+
+  function decideMode() {
+    mode = prompt(
+      "Select difficulty (type easy, medium, or hard):\nType quit to stop."
+    );
+    hasQuit(mode);
+
+    if (isPlaying && !modes.includes(mode)) {
+      decideMode();
+    }
+  }
 }
